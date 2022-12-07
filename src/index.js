d50fb2c60fe3b31bf4b30ab67aca72a7cf6a0d0a
wooka000 <wooka000@naver.com>
2022-12-08 00:38:18 +0900

    setting Router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import NotFound from "./pages/NotFound";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import VideoDetail from "./pages/VideoDetail";
+import Videos from "./pages/Videos";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElemen: <NotFound />,
+    childeren: [
+      { index: true, element: <Videos /> },
+      { path: "videos", element: <Videos /> },
+      { path: "videos/:keyword", element: <Videos /> },
+      { path: "videos/:keyword/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

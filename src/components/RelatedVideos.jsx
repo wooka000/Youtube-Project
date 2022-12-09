fc33d283167f14632301a8e5d6a1ac02a2f52f70
wooka000 <wooka000@naver.com>
2022-12-09 23:33:24 +0900

    updatd : detail page

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -1,26 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import { useParams } from "react-router-dom";
-import VideoCard from "../components/VideoCard";
 import { useYoutubeApi } from "../context/YoutubeApiContext";
+import VideoCard from "./VideoCard";
 
-export default function Videos() {
-  const { keyword } = useParams();
+export default function RelatedVideos({ id }) {
   const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => youtube.search(keyword));
-
+  } = useQuery(["related", id], () => youtube.relatedVideos(id));
   return (
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong</p>}
       {videos && (
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
+        <ul className="cursor-pointer">
           {videos.map((video) => (
-            <VideoCard key={video.id} video={video} />
+            <VideoCard key={video.id} video={video} type="list" />
           ))}
         </ul>
       )}
